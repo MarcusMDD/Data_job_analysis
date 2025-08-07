3184f36a90507dee755ba02b7ec8ef131d7aa7e2
MarcusMDD <marcdarr06@gmail.com>
2025-08-07 15:00:21 +0100

    Add files via upload
    Updated to also query for the name of the company posting the job

---
+++ 1_top_paying_jobs.sql
@@ -0,0 +1,28 @@
+/*
+Question: What are the top-paying Data Analyst jobs 
+-Identify the top 10 paying data Analyst jobs available remotely.
+-Focuses on job postings with specified salaries (Ignore null values)
+-Why? Highlights the top-paying oppurtunities for Data Analysts, offering insights into exmployment options 
+*/
+
+SELECT
+    job_id,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date,
+    name AS company_name 
+FROM 
+    job_postings_fact 
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+--This above join allows us to also query for the names of the companies posting the jobs--
+WHERE
+     job_title_short = 'Data Analyst' AND
+     job_location = 'Anywhere' AND 
+     salary_year_avg IS NOT NULL 
+-- The above where statements filters all jobs down to remote Data Analyst jobs, with Salary Data-- 
+ORDER BY salary_year_avg DESC
+--This orders all of the job postings by yearly salary from highest to lowest--
+LIMIT 10 
+--This limits the reuslts to the top 10--
